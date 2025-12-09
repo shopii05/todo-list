70cd7c96e637f77b10a254c564b3d80389fbb520
shopii05 <carmen.herrera19@soy.sena.edu.co>
2025-12-09 17:06:41 -0500

    fix vite base path for render

--- Todo-List-React-develop/frontend/vite.config.js
+++ Todo-List-React-develop/frontend/vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default defineConfig({  
+export default defineConfig({
   plugins: [react()],
-  base: '/Todo-List-React-1/'
-})
+  base: "/",   // ✅ ESTA LÍNEA ES LA CLAVE
+});
